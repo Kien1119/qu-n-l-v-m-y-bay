d2f7a23ff9d821b4b273ebe30e26932cdb3d7c0a
Kiennd <me@kiennd.io.vn>
2024-10-06 00:33:48 +0700

    add  popup comfirm

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <ConfirmPopup></ConfirmPopup>
     <component :is="layout">
       <RouterView></RouterView>
     </component>
@@ -10,6 +11,7 @@
 import { computed } from 'vue'
 import DefaultPage from './layouts/DefaultPage.vue'
 import NormalPage from './layouts/NormalPage.vue'
+import ConfirmPopup from 'primevue/confirmpopup';
 
 import { useRoute } from 'vue-router'
 const route = useRoute()
